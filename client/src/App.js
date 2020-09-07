5d4400147a6e3191714f2d7e40ad0a652a275d88
Hernan <hernanpesantez@me.com>
2020-09-07 09:30:09 -0400

    switching work space

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar'
+
+
+
+class App extends Component{
+
+
+
+  render(){
+
+    return (
+
+        <Fragment>
+          
+          <Navbar  />
+
+        </Fragment>
+
+
+    ); 
+
+  }
+
+}
+
+export default App;
